8d1cb227ecb345120192fd0e3f1b3ef381749e10
PFranklinM <paulmallon@Pauls-MacBook-Pro.local>
2015-12-15 00:18:31 -0500

    CC Lab Final Commit 1

--- of_v0.8.4_osx_release/apps/myApps/OF_Final/src/ofApp.cpp
+++ of_v0.8.4_osx_release/apps/myApps/OF_Final/src/ofApp.cpp
@@ -8,8 +8,8 @@ void ofApp::setup(){
     
     ofSetFrameRate(60);
     
-    camW = 1280;
-    camH = 720;
+    camW = ofGetWidth();
+    camH = ofGetHeight();
     
     w = camW;
     h = camH;
@@ -44,6 +44,18 @@ void ofApp::setup(){
     xVel = 5;
     yVel = -5;
     
+    goalieVelP1A = 5;
+    goalieVelP1B = 5;
+    
+    goalieVelP2A = 5;
+    goalieVelP2B = 5;
+
+    goalieP1A.set(ofGetWidth()*0.05, ofGetHeight()*0.25);
+    goalieP1B.set(ofGetWidth()*0.05, ofGetHeight()*0.75);
+    
+    goalieP2A.set(ofGetWidth()*0.95, ofGetHeight()*0.25);
+    goalieP2B.set(ofGetWidth()*0.95, ofGetHeight()*0.75);
+    
     counter = 0;
     
     xMin = 0;
@@ -55,7 +67,9 @@ void ofApp::setup(){
     player1Score = 0;
     player2Score = 0;
     
-    isHit = false;
+    movingRight = true;
+    
+    movingLeft = false;
 
 
 
@@ -97,37 +111,146 @@ void ofApp::update(){
     
     int x, y;
     
+    if(xVel < 0){
+        movingLeft = true;
+        movingRight = false;
+    }
+    
+    if(xVel > 0){
+        movingLeft = false;
+        movingRight = true;
+    }
+    
+    if(xVel == 0){
+        movingLeft = true;
+        movingRight = true;
+    }
+    
     for (int i = 0; i < contourFinder.nBlobs; i++){
         
-        contourFinder.blobs[i].draw(0, 0);
+        contourFinder.blobs[i].draw(camW, camH);
         
         x = (contourFinder.blobs[i].centroid.x);
         y = (contourFinder.blobs[i].centroid.y);
         
         motion.set(x, y);
         
-        ofVec3f diff = motion - pos;
+        int sumXP1 = 0;
+        int sumYP1 = 0;
+        int sumAreaP1 = 0;
         
-        if(diff.length() < 50){
-            
-            if(ofGetElapsedTimeMillis() >= 100){
-                float percent = 1 - (diff.length() / 50);
+        int sumXP2 = 0;
+        int sumYP2 = 0;
+        int sumAreaP2 = 0;
+        
+        
+        for (int blob1 = 0; blob1 <= contourFinder.getWidth()*0.25; blob1++) {
+                    
+            float xposP1;
+            float yposP1;
                 
-                xVel += diff.x * percent;
+            xposP1 = contourFinder.blobs[i].centroid.x;
+            yposP1 = contourFinder.blobs[i].centroid.y;
+
+            float areaBlobP1 = contourFinder.blobs[i].area;
+                
+            sumXP1 += xposP1 * areaBlobP1;
+            
+            sumYP1 += yposP1 * areaBlobP1;
+                    
+            sumAreaP1 += areaBlobP1;
+        
+        }
+        
+            float averageXP1 = sumXP1/sumAreaP1;
+            float averageYP1 = sumYP1/sumAreaP1;
+        
+            P1.set(averageXP1, averageYP1);
+        
+        
+        for (auto blob2 = ofGetWidth()*0.75; blob2 <= ofGetWidth(); blob2++) {
+            
+            float xposP2;
+            float yposP2;
+            
+            xposP2 = contourFinder.blobs[i].centroid.x;
+            yposP2 = contourFinder.blobs[i].centroid.y;
+            
+            float areaBlobP2 = contourFinder.blobs[i].area;
+            
+            sumXP2 += xposP2 * areaBlobP2;
+            
+            sumYP2 += yposP2 * areaBlobP2;
+            
+            sumAreaP2 += areaBlobP2;
+            
+    }
+
+    
+    float averageXP2 = sumXP2/sumAreaP2;
+    float averageYP2 = sumYP2/sumAreaP2;
+    
+    P2.set(averageXP2, averageYP2);
+
+    
+    
+        ofVec2f diffP1 = P1 - pos;
+        ofVec2f diffP2 = P2 - pos;
+    
+        if(diffP1.length() < 50 || diffP2.length() < 50){
+
+            if(movingRight == true && pos.x > rightSide){
+                
+                float percentP2 = 1 - (diffP2.length() / 65);
+                
+                xVel += diffP2.x * percentP2;
                 xVel = -xVel;
                 
-                yVel += diff.y * percent;
+                yVel += diffP2.y * percentP2;
                 yVel = -yVel;
                 
-                isHit = true;
             }
             
-            if(isHit == true){
-                isHit = false;
-                ofResetElapsedTimeCounter();
+            if(movingLeft == true && pos.x < leftSide){
+            
+                float percentP1 = 1 - (diffP1.length() / 65);
+                
+                xVel += diffP1.x * percentP1;
+                xVel = -xVel;
+                
+                yVel += diffP1.y * percentP1;
+                yVel = -yVel;
             }
+            
         }
+    
+    ofVec2f goalieDiffP1A = goalieP1A - pos;
+    ofVec2f goalieDiffP1B = goalieP1B - pos;
+    
+    ofVec2f goalieDiffP2A = goalieP2A - pos;
+    ofVec2f goalieDiffP2B = goalieP2B - pos;
+    
+    if(goalieDiffP1A.length() < 50 || goalieDiffP1B.length() < 50 ||
+       goalieDiffP2A.length() < 50 || goalieDiffP2B.length() < 50){
+        
+        if(movingRight == true && pos.x > rightSide){
+            
+            xVel = -xVel;
+            
+            yVel = -yVel;
+            
+        }
+        
+        if(movingLeft == true && pos.x < leftSide){
+
+            xVel = -xVel;
+        
+            yVel = -yVel;
+        }
+
+        
     }
+}
 
     pos.x += xVel;
     pos.y -= yVel;
@@ -154,9 +277,31 @@ void ofApp::update(){
         yVel = -yVel;
     }
     
+    goalieP1A.y -= goalieVelP1A;
+    goalieP1B.y += goalieVelP1B;
+    
+    goalieP2A.y -= goalieVelP2A;
+    goalieP2B.y += goalieVelP2B;
+    
+    
+    if(goalieP1A.y >= ofGetHeight()*0.5 || goalieP1A.y <= yMin){
+        goalieVelP1A = -goalieVelP1A;
+    }
+    
+    if(goalieP1B.y <= ofGetHeight()*0.5 || goalieP1B.y >= yMax){
+        goalieVelP1B = -goalieVelP1B;
+    }
+    
+    if(goalieP2A.y >= ofGetHeight()*0.5 || goalieP2A.y <= yMin){
+        goalieVelP2A = -goalieVelP2A;
+    }
+    
+    if(goalieP2B.y <= ofGetHeight()*0.5 || goalieP2B.y >= yMax){
+        goalieVelP2B = -goalieVelP2B;
+    }
+
+    
     ofSetColor(255, 255, 255);
-
-
 
 }
 
@@ -181,14 +326,48 @@ void ofApp::draw(){
     ofSetColor(255, 0, 0);
     ofNoFill();
     
-    ofRect(ofGetWidth()*0.25, 0, 20, ofGetHeight());
-    ofRect(ofGetWidth()*0.75, 0, 20, ofGetHeight());
+    ofRect(ofGetWidth()*0.33, 0, 20, ofGetHeight());
+    ofRect(ofGetWidth()*0.66, 0, 20, ofGetHeight());
     
     ofSetColor(255);
-//    ofScale(500, 500);
     ofDrawBitmapString("Player 1: " + ofToString(player1Score), ofGetWidth()*0.05, ofGetHeight()*0.05);
     ofDrawBitmapString("Player 2: " + ofToString(player2Score), ofGetWidth()*0.9, ofGetHeight()*0.05);
+    
+    if(player1Score >= 10){
+        ofSetColor(255);
+        ofFill();
+        ofRect(goalieP2A.x, goalieP2A.y, 20, 100);
+    }
+    
+    if(player1Score >= 20){
+        ofSetColor(255);
+        ofFill();
+        ofRect(goalieP2B.x, goalieP2B.y, 20, 100);
+    }
+    
+    if(player2Score >= 10){
+        ofSetColor(255);
+        ofFill();
+        ofRect(goalieP1A.x, goalieP1A.y, 20, 100);
+    }
+    
+    if(player2Score >= 20){
+        ofSetColor(255);
+        ofFill();
+        ofRect(goalieP1B.x, goalieP1B.y, 20, 100);
+    }
 
+    if(motion.x <= ofGetWidth()*0.25 && movingLeft == true){
+        ofSetColor(0, 0, 255);
+        ofFill();
+        ofRect(P1.x, P1.y, 50, 250);
+    }
+    
+    if(motion.x >= ofGetWidth()*0.75 && movingRight == true){
+        ofSetColor(0, 255, 0);
+        ofFill();
+        ofRect(P2.x, P2.y, 50, 250);
+    }
 
 }
 
--- of_v0.8.4_osx_release/apps/myApps/OF_Final/src/ofApp.h
+++ of_v0.8.4_osx_release/apps/myApps/OF_Final/src/ofApp.h
@@ -22,8 +22,8 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
     
-    int camW= 1280;
-    int camH= 720;
+    int camW;
+    int camH;
   
 
     
@@ -39,9 +39,9 @@ class ofApp : public ofBaseApp{
     
     ofxCvContourFinder contourFinder;
     
-    bool			isMotionDetected;
+    bool isMotionDetected;
     
-    bool			drawGui;
+    bool drawGui;
     
     ofxGuiGroup gui;
     ofxLabel guiHelp;
@@ -64,16 +64,39 @@ class ofApp : public ofBaseApp{
     int xVel;
     int yVel;
     
+    int goalieVelP1A;
+    int goalieVelP1B;
+    
+    int goalieVelP2A;
+    int goalieVelP2B;
+    
     int player1Score;
     int player2Score;
     
     int counter;
     
-    bool isHit;
+//    bool isHit;
     
     ofVec2f motion;
     
     ofVec2f pos;
+    
+    ofVec2f P1;
+    ofVec2f P2;
+    
+    ofVec2f goalieP1A;
+    ofVec2f goalieP1B;
+    
+    ofVec2f goalieP2A;
+    ofVec2f goalieP2B;
+    
+    
+    bool movingRight;
+    bool movingLeft;
+    
+    int rightSide = ofGetWidth()*0.66;
+    int leftSide = ofGetWidth()*0.33;
+    
 
 		
 };
